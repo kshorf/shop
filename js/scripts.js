a219c66e1aa2eccc9bb2a6da8e375ed631e65ea6
jacob ruleaux and Kaden Shorf <jacob@>
2017-06-05 16:24:18 -0700

    add input and ul and submit btn

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   $("#form1").submit(function(event) {
- var lists = ["item1", "item2", "item3", "item4"];
+ var lists = ["#item1", "#item2", "#item3", "#item4"];
  var upper = lists.map(function(list) {
   return list.toUpperCase();
  upper.sort();
-
+ $("." + upper).text();
+$("#stuff").show(upper);
     event.preventDefault();
   });
+});
 });
