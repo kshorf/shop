0940d5b2d1ab222bd2132c6abfe30aa4bf7f8ee4
jacob ruleaux and Kaden Shorf <jacob@>
2017-06-05 16:52:06 -0700

    add more functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
   $("#form1").submit(function(event) {
  var lists = ["#item1", "#item2", "#item3", "#item4"];
+
+ lists.forEach(function(list) {
+   var userInput = $("input#" + list).val();
+   $("." + list).text(userInput);
+ });
  var upper = lists.map(function(list) {
   return list.toUpperCase();
  upper.sort();
- $("." + upper).text();
-$("#stuff").show(upper);
+
+ $("#stuff").show();
     event.preventDefault();
   });
 });
